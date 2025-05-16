d75074b36f6b6107eba5d00972fc4f72c1f296c2
atakuya-12915 <t.taku0402@outlook.com>
2025-05-17 02:52:00 +0900

    模範解答を追記

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,16 @@
-const today = () => {
-  const date = new Date();
-  return `${date.getFullYear()} 年 ${
-    date.getMonth() + 1
-  } 月 ${date.getDate()} 日`;
-};
+// const today = () => {
+//   const date = new Date();
+//   return `${date.getFullYear()} 年 ${
+//     date.getMonth() + 1
+//   } 月 ${date.getDate()} 日`;
+// };
 
-console.log(today());
+// console.log(today());
+
+// 模範回答
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1; //月はindexでは0から始まるため+1する
+const date = today.getDate();
+
+console.log(year + "年" + month + "月" + date + "日");
